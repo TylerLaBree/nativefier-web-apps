d397a1042a7881b38440e1175f33ad330186877a
Tyler LaBree <tylerlabree@github.com>
2023-05-03 13:41:07 -0500

    Open programs in wayland instead of xwayland

--- tools/discord.sh
+++ tools/discord.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 nativefier 'discord.com/app' --name 'Discord' --background-color '#36393f' --icon '../icons/discord.svg' --single-instance
 cp ../icons/discord.svg Discord-linux-x64/resources/app/icon.svg
-cp ../applications/discord.desktop /usr/share/applications/
-cp -r Discord-linux-x64 /opt/
+sudo cp ../applications/discord.desktop /usr/share/applications/
+sudo cp -r Discord-linux-x64 /opt/
 rm -rf Discord-linux-x64
--- tools/proton-calendar.sh
+++ tools/proton-calendar.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 nativefier 'calendar.proton.me/' --name 'Proton Calendar' --background-color '#16141c' --icon '../icons/proton-calendar.png' --single-instance
 cp ../icons/proton-calendar.png ProtonCalendar-linux-x64/resources/app/icon.png
-cp ../applications/proton-calendar.desktop /usr/share/applications/
-cp -r ProtonCalendar-linux-x64 /opt/
+sudo cp ../applications/proton-calendar.desktop /usr/share/applications/
+sudo cp -r ProtonCalendar-linux-x64 /opt/
 rm -rf ProtonCalendar-linux-x64
